5725ef92ad5cbd29c5343284f5bc08d692fb6751
Suraj <snair306@gmail.com>
2023-05-29 18:13:51 -0500

    all that needs to be done is adding previous search results into buttons and getting the icons to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,26 @@ var currentCity = document.getElementById('city')
 var currentCityWeather = document.getElementById('cityweather')
 var forecastcity = document.getElementById('5daycurrentcity')
 var forecastCards = document.querySelectorAll('#five-day .card');
-
+var pastSearches = localStorage.getItem('history')
+var emptyArray = []
+emptyArray.push(pastSearches);
+console.log(emptyArray);
 
 var handleSearch = function(event) {
     event.preventDefault();
+    
 
-    var userInput = qInput.value;
-    console.log(userInput);
+    var userInput = qInput.value.toLowerCase();
+    // console.log(userInput);
+    console.log(pastSearches);
+    if (emptyArray.includes(userInput) === false) {
+        emptyArray.push(userInput);
+        localStorage.setItem('history', emptyArray);
+
+    }
+   
+
+    
 
     var firstAPIURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=c055c2d07b3173d39c878322108c0189'
     
@@ -24,41 +37,49 @@ var handleSearch = function(event) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
+            // console.log(data);
             var lat = data[0].lat
             var lon = data[0].lon
-            console.log(lat, lon);
+            // console.log(lat, lon);
 
             var secondAPIURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=c055c2d07b3173d39c878322108c0189&units=imperial'
             // console.log(secondAPIURL, 'working');
+            
 
             fetch(secondAPIURL)
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function(weatherData) {
-                    // console.log(weatherData);
-                    console.log('Temp' + ' ' + weatherData.main.temp + '°F','Humidity' + ' ' + weatherData.main.humidity + ' ' + '%','Wind' + ' ' + weatherData.wind.speed + ' ' +'MPH', weatherData.name);
+                    console.log(weatherData, 'working');
+                    // console.log('Temp' + ' ' + weatherData.main.temp + '°F','Humidity' + ' ' + weatherData.main.humidity + ' ' + '%','Wind' + ' ' + weatherData.wind.speed + ' ' +'MPH', weatherData.name);
                 
-
 
                 var temperature = weatherData.main.temp;
                 var humidity = weatherData.main.humidity;
                 var wind = weatherData.wind.speed;
                 var city = weatherData.name;
-                console.log(temperature, humidity, wind, city);
+                var currentIcon = weatherData.weather[0].icon
+                console.log(currentIcon);
+                var iconURL = document.createElement('img');
+                iconURL.src =  'https://openweathermap.org/img/wn/' + currentIcon + '@2x.png'
+                currentCity.innerHTML = ''
+                currentCity.appendChild(iconURL);
 
-                currentCity.textContent = city
+                // console.log(temperature, humidity, wind, city);
+                currentCity.textContent = city + ' ' + dayjs().format('(M/D/YYYY)')
                 currentCityWeather.textContent ='Temp: ' + temperature + '°F' + '   ' + 'Wind: ' + wind + 'MPH' + '  ' + 'Humidity: ' + humidity + '%' 
                 searchedEl.textContent = city
                 forecastcity.textContent = city
+                
+                // console.log(dayjs().format('M,D,YYYY'), 'working');
             })
 
 
 
             var thirdAPIURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=c055c2d07b3173d39c878322108c0189&units=imperial'
 
-            console.log(thirdAPIURL,'5day');
+            // console.log(thirdAPIURL,'5day');
 
             fetch(thirdAPIURL)
             .then(function(response) {
@@ -66,10 +87,10 @@ var handleSearch = function(event) {
             })
             .then(function(forecastData) {
                 for (let i = 0; i < 5; i++) {
-//                    var card = forecastCards[i];
-                    // var forecast = forecastData.list[i];
+                   var card = forecastCards[i];
+                    var forecast = forecastData.list[i * 8];
 
-                    console.log(forecastData);
+                    // console.log(forecastData, 'hello');
 
                     var title = card.querySelector('.card-title');
                     var subtitle = card.querySelector('.card-subtitle');
@@ -78,8 +99,8 @@ var handleSearch = function(event) {
 
 
                     title.textContent = 'Date: ' + forecast.dt_txt.slice(5, 10);
-                    subtitle.textContent = 'Wind: ' + forecast.wind.speed + ' MPH';
-                    text.textContent = 'Humidity: ' + forecast.main.humidity + '%, Temp: ' + forecast.main.temp + '°F';
+                    subtitle.textContent = 'Temp: ' + forecast.main.temp + '°F';
+                    text.textContent = 'Humidity: ' + forecast.main.humidity + '%' + ' ' + 'Wind: ' + forecast.wind.speed + 'MPH';
                     // console.log(forecast.dt_txt, "wokring");
 
                 }
